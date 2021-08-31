d6e97d8a5976c193d00a824a51e624e15141cbfa
Optix™ <zecmad16@htlkaindorf.at>
2021-08-31 08:44:08 +0200

    Implemented Logging Utility

--- Chroma64/Emulator/Memory/ROM.cs
+++ Chroma64/Emulator/Memory/ROM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chroma64.Util;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace Chroma64.Emulator.Memory
                 {
                     // Native big endian format (ABCD)
                     case 0x40123780:
-                        Console.WriteLine("[ROM Loader] Format: Native");
+                        Log.Info("Format: Native");
                         break;
 
                     // Byte-swapped format (BADC)
@@ -31,7 +32,7 @@ namespace Chroma64.Emulator.Memory
                             romPtr[i] = romPtr[i + 1];
                             romPtr[i + 1] = tmp;
                         }
-                        Console.WriteLine("[ROM Loader] Format: Byte-swapped");
+                        Log.Info("Format: Byte-swapped");
                         break;
 
                     // Little endian format (DCBA)
@@ -45,12 +46,12 @@ namespace Chroma64.Emulator.Memory
                             romPtr[i + 2] = romPtr[i + 1];
                             romPtr[i + 1] = tmp;
                         }
-                        Console.WriteLine("[ROM Loader] Format: Little-endian");
+                        Log.Info("Format: Little-endian");
                         break;
 
                     // Some other unknown format or a file that's not a ROM, error
                     default:
-                        Console.Error.WriteLine("[ROM Loader] Unknown ROM format");
+                        Log.Error("Unknown ROM format");
                         break;
                 }
 
@@ -58,9 +59,9 @@ namespace Chroma64.Emulator.Memory
                 Array.Reverse(bytes);
 
                 // Extra validation + logging
-                Console.WriteLine($"[ROM Loader] Header: 0x{Read<uint>(0).ToString("X4")}");
+                Log.Info($"Header: 0x{Read<uint>(0).ToString("X4")}");
                 if ((*(uint*)(romPtr + bytes.Length - 4)) != 0x80371240)
-                    throw new Exception("[ROM Loader] Invalid ROM File!");
+                    Log.FatalError("Invalid ROM File!");
             }
         }
     }
---
+++ Chroma64/Util/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Chroma64.Util
+{
+    static class Log
+    {
+        public static void Info(string msg)
+        {
+            Console.WriteLine($"[{NameOfCallingClass()}] {msg}");
+        }
+        public static void Error(string msg)
+        {
+            Console.Error.WriteLine($"[{NameOfCallingClass()}] {msg}");
+        }
+
+        public static void FatalError(string msg)
+        {
+            Console.Error.WriteLine($"[{NameOfCallingClass()} - FATAL] {msg}");
+            Console.ReadKey();
+            Environment.Exit(-1);
+        }
+
+        private static string NameOfCallingClass()
+        {
+            string fullName;
+            Type declaringType;
+            int skipFrames = 2;
+            do
+            {
+                MethodBase method = new StackFrame(skipFrames, false).GetMethod();
+                declaringType = method.DeclaringType;
+                if (declaringType == null)
+                {
+                    return method.Name;
+                }
+                skipFrames++;
+                fullName = declaringType.FullName;
+            }
+            while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
+
+            return fullName.Substring(fullName.LastIndexOf('.') + 1);
+        }
+    }
+}
