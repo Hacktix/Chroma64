984561acf71411fecad22477032510b4b8668f09
Hacktix <zecmad16@htlkaindorf.at>
2021-08-31 19:00:16 +0200

    Implemented setup for branching + BNE Instruction

--- Chroma64/Emulator/CPU/MainCPU.cs
+++ Chroma64/Emulator/CPU/MainCPU.cs
@@ -20,8 +20,12 @@ namespace Chroma64.Emulator.CPU
 
         private Dictionary<uint, Action<uint>> instrs = new Dictionary<uint, Action<uint>>();
         private Dictionary<uint, Action<uint>> instrsSpecial = new Dictionary<uint, Action<uint>>();
-        private Dictionary<uint, Action<uint>> instrsBranch = new Dictionary<uint, Action<uint>>();
+        private Dictionary<uint, Action<uint>> instrsRegimm = new Dictionary<uint, Action<uint>>();
         private Dictionary<uint, Action<uint>> instrsCop = new Dictionary<uint, Action<uint>>();
+
+        // Branch Instruction Variables
+        private int branchQueued = 0;
+        private ulong branchTarget;
 
         public MainCPU(MemoryBus bus)
         {
@@ -52,7 +56,8 @@ namespace Chroma64.Emulator.CPU
 
             instrs = new Dictionary<uint, Action<uint>>()
             {
-                { 0, InstrSpecial }, { 1, InstrBranch }, { 16, InstrCop }, { 17, InstrCop }, { 18, InstrCop },
+                { 0, InstrSpecial }, { 1, InstrRegimm }, { 16, InstrCop }, { 17, InstrCop }, { 18, InstrCop },
+                { 5, MIPS_BNE },
                 { 15, MIPS_LUI }, { 35, MIPS_LW },
                 { 9, MIPS_ADDIU },
             };
@@ -67,17 +72,26 @@ namespace Chroma64.Emulator.CPU
         {
             for (int i = 0; i < cycles; i++)
             {
+                // Fetch & increment PC
                 uint instr = bus.Read<uint>(pc);
                 pc += 4;
 
-                uint opcode = (instr & 0xFC000000) >> 26;
-                if (instrs.ContainsKey(opcode))
-                    instrs[opcode](instr);
-                else
+                // Decode opcode & execute
+                if(instr != 0)
                 {
-                    pc -= 4;
-                    Log.FatalError($"Unimplemented Instruction 0x{instr:X8} [Opcode {opcode}] at PC = 0x{pc:X16}");
+                    uint opcode = (instr & 0xFC000000) >> 26;
+                    if (instrs.ContainsKey(opcode))
+                        instrs[opcode](instr);
+                    else
+                    {
+                        pc -= 4;
+                        Log.FatalError($"Unimplemented Instruction 0x{instr:X8} [Opcode {opcode}] at PC = 0x{pc:X16}");
+                    }
                 }
+
+                // Handle queued branches
+                if(branchQueued > 0 && --branchQueued == 0)
+                    pc = branchTarget;
             }
         }
 
@@ -112,11 +126,11 @@ namespace Chroma64.Emulator.CPU
             }
         }
 
-        private void InstrBranch(uint instr)
+        private void InstrRegimm(uint instr)
         {
             uint opcode = (instr & 0x1F0000) >> 16;
-            if (instrsBranch.ContainsKey(opcode))
-                instrsBranch[opcode](instr);
+            if (instrsRegimm.ContainsKey(opcode))
+                instrsRegimm[opcode](instr);
             else
             {
                 pc -= 4;
@@ -137,7 +151,7 @@ namespace Chroma64.Emulator.CPU
         }
         #endregion
 
-        #region Instruction Implementations
+
 
         #region Normal Instructions
         void MIPS_LUI(uint instr)
@@ -172,10 +186,24 @@ namespace Chroma64.Emulator.CPU
 
             LogInstr("LW", $"{src} -> {baseAddr:X16} + {offset:X4} -> [{addr:X16}] -> {val:X4} -> {dest}");
         }
-        #endregion
 
-        #region Special Instructions
+        void MIPS_BNE(uint instr)
+        {
+            CPUREG src = (CPUREG)((instr & 0x3E00000) >> 21);
+            CPUREG dest = (CPUREG)((instr & 0x1F0000) >> 16);
+            ulong offset = (ulong)(((short)(instr & 0xFFFF)) << 2);
+            ulong addr = pc + offset;
+            long val1 = GetReg(src);
+            long val2 = GetReg(dest);
+            bool cond = val1 != val2;
+            if(cond)
+            {
+                branchQueued = 2;
+                branchTarget = addr;
+            }
 
+            LogInstr("BNE", $"{src} == {dest} -> {val1:X16} == {val2:X16} -> {(cond ? "" : "No ")}Branch to {addr:X8}");
+        }
         #endregion
 
         #region Coprocessor Instructions
@@ -189,6 +217,5 @@ namespace Chroma64.Emulator.CPU
         }
         #endregion
 
-        #endregion
     }
 }
