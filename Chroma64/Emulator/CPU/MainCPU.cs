7b25ba27470159b740fae55781abd37aff80ef73
Optix™ <zecmad16@htlkaindorf.at>
2021-08-31 15:02:17 +0200

    Implemented MTC0, LUI, ADDIU, LW

--- Chroma64/Emulator/CPU/MainCPU.cs
+++ Chroma64/Emulator/CPU/MainCPU.cs
@@ -49,21 +49,18 @@ namespace Chroma64.Emulator.CPU
             cop0.Registers[16] = 0x0006E463;
 
             // # Initializing Instruction LUT
+
             instrs = new Dictionary<uint, Action<uint>>()
             {
                 { 0, InstrSpecial }, { 1, InstrBranch }, { 16, InstrCop }, { 17, InstrCop }, { 18, InstrCop },
+                { 15, MIPS_LUI }, { 35, MIPS_LW },
+                { 9, MIPS_ADDIU },
             };
-        }
 
-        private void SetReg(CPUREG reg, long value)
-        {
-            if (reg != CPUREG.ZERO)
-                regs[(int)reg] = value;
-        }
-
-        private long GetReg(CPUREG reg)
-        {
-            return regs[(int)reg];
+            instrsCop = new Dictionary<uint, Action<uint>>()
+            {
+                { 4, MIPS_MTC0 },
+            };
         }
 
         public void Tick(int cycles)
@@ -83,6 +80,24 @@ namespace Chroma64.Emulator.CPU
                 }
             }
         }
+
+        private void LogInstr(string instr, string msg)
+        {
+            Log.Info($"[PC = 0x{pc - 4:X8}] {instr.PadRight(6)} : {msg}");
+        }
+
+        #region CPU Register Instructions
+        private void SetReg(CPUREG reg, long value)
+        {
+            if (reg != CPUREG.ZERO)
+                regs[(int)reg] = value;
+        }
+
+        private long GetReg(CPUREG reg)
+        {
+            return regs[(int)reg];
+        }
+        #endregion
 
         #region Sub-Instruction Decoders
         private void InstrSpecial(uint instr)
@@ -120,6 +135,60 @@ namespace Chroma64.Emulator.CPU
                 Log.FatalError($"Unimplemented Coprocessor Instruction 0x{instr:X8} [Opcode {opcode}] at PC = 0x{pc:X16}");
             }
         }
+        #endregion
+
+        #region Instruction Implementations
+
+        #region Normal Instructions
+        void MIPS_LUI(uint instr)
+        {
+            CPUREG dest = (CPUREG)((instr & 0x1F0000) >> 16);
+            long val = (int)((instr & 0xFFFF) << 16);
+            SetReg(dest, val);
+
+            LogInstr("LUI", $"{val:X16} -> {dest}");
+        }
+
+        void MIPS_ADDIU(uint instr)
+        {
+            CPUREG src = (CPUREG)((instr & 0x3E00000) >> 21);
+            CPUREG dest = (CPUREG)((instr & 0x1F0000) >> 16);
+            long val = (int)((instr & 0xFFFF) << 16);
+            long regval = GetReg(src);
+            SetReg(dest, regval + val);
+
+            LogInstr("ADDIU", $"{src} -> {regval:X16} + {val:X16} -> {regval+val:X16} -> {dest}");
+        }
+
+        void MIPS_LW(uint instr)
+        {
+            CPUREG src = (CPUREG)((instr & 0x3E00000) >> 21);
+            CPUREG dest = (CPUREG)((instr & 0x1F0000) >> 16);
+            short offset = (short)((instr & 0xFFFF) << 16);
+            long baseAddr = GetReg(src);
+            ulong addr = (ulong)(baseAddr + offset);
+            short val = bus.Read<short>(addr);
+            SetReg(dest, val);
+
+            LogInstr("LW", $"{src} -> {baseAddr:X16} + {offset:X4} -> [{addr:X16}] -> {val:X4} -> {dest}");
+        }
+        #endregion
+
+        #region Special Instructions
+
+        #endregion
+
+        #region Coprocessor Instructions
+        void MIPS_MTC0(uint instr)
+        {
+            COP0REG dest = (COP0REG)((instr & 0xF800) >> 11);
+            CPUREG src = (CPUREG)((instr & 0x1F0000) >> 16);
+            cop0.SetReg(dest, GetReg(src));
+
+            LogInstr("MTC0", $"{src} -> {GetReg(src):X16} -> {dest}");
+        }
+        #endregion
+
         #endregion
     }
 }
