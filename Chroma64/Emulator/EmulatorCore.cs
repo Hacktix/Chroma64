110fe7ab517eddcb7cf177f2a085dd82e6861a00
Optix™ <zecmad16@htlkaindorf.at>
2021-08-31 08:07:46 +0200

    Implemented basic main loop

---
+++ Chroma64/Emulator/EmulatorCore.cs
@@ -0,0 +1,24 @@
+﻿using Chroma64.Emulator.Memory;
+
+namespace Chroma64.Emulator
+{
+    class EmulatorCore
+    {
+        private static readonly int TICKS_PER_FRAME = 1562500;
+
+        private ROM _rom;
+
+        public EmulatorCore(string romPath)
+        {
+            _rom = new ROM(romPath);
+        }
+
+        public void TickFrame()
+        {
+            for (int i = 0; i < TICKS_PER_FRAME; i++)
+            {
+                // TODO: Tick components here
+            }
+        }
+    }
+}
