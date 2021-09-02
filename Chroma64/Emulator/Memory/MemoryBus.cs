37a8e028dac5558a033e96429b09567b7a3434e5
Optix™ <zecmad16@htlkaindorf.at>
2021-09-02 09:17:35 +0200

    Stubbed Peripheral Interface

--- Chroma64/Emulator/Memory/MemoryBus.cs
+++ Chroma64/Emulator/Memory/MemoryBus.cs
@@ -10,6 +10,7 @@ namespace Chroma64.Emulator.Memory
         public BigEndianMemory SP_IMEM = new BigEndianMemory(0x1000);
 
         public RDRAMInterface RI = new RDRAMInterface();
+        public PeripheralInterface PI = new PeripheralInterface();
 
         private ROM rom;
 
@@ -34,6 +35,10 @@ namespace Chroma64.Emulator.Memory
             else if (addr >= 0x04001000 && addr <= 0x04001FFF)
                 return SP_IMEM.Read<T>(addr & 0xFFF);
 
+            // Peripheral Interface
+            else if (addr >= 0x04600000 && addr <= 0x046FFFFF)
+                return PI.Read<T>(addr & 0xFFFFF);
+
             // RDRAM Interface
             else if (addr >= 0x04700000 && addr <= 0x047FFFFF)
                 return RI.Read<T>(addr & 0xFFFFF);
@@ -52,19 +57,23 @@ namespace Chroma64.Emulator.Memory
 
             // RDRAM (Built-in)
             if (addr >= 0x00000000 && addr <= 0x003FFFFF)
-                RDRAM.Write<T>(addr & 0x3FFFFF, val);
+                RDRAM.Write(addr & 0x3FFFFF, val);
 
             // SP DMEM
             else if (addr >= 0x04000000 && addr <= 0x04000FFF)
-                SP_DMEM.Write<T>(addr & 0xFFF, val);
+                SP_DMEM.Write(addr & 0xFFF, val);
 
             // SP IMEM
             else if (addr >= 0x04001000 && addr <= 0x04001FFF)
-                SP_IMEM.Write<T>(addr & 0xFFF, val);
+                SP_IMEM.Write(addr & 0xFFF, val);
+
+            // Peripheral Interface
+            else if (addr >= 0x04600000 && addr <= 0x046FFFFF)
+                PI.Write(addr & 0xFFFFF, val);
 
             // RDRAM Interface
             else if (addr >= 0x04700000 && addr <= 0x047FFFFF)
-                RI.Write<T>(addr & 0xFFFFF, val);
+                RI.Write(addr & 0xFFFFF, val);
 
             else
                 Log.CriticalError($"Write to unknown address 0x{addr:X8}");
